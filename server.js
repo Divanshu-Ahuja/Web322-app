00889e5579306d3a63583d1fa7c9fb5d8d507f49
Your Name <you@example.com>
2023-01-30 14:26:15 -0500

    commit

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+const express = require("express");
+var path = require("path");
+const app = express();
+var HTTP_PORT = process.env.PORT || 8080;
+
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
+app.get("/", function (req, res) {
+  res.send("Hello World<br /><a href='/about'>Go to the about page</a>");
+});
+
+app.get("/about", function (req, res) {
+  res.sendFile(path.join(_dirname, "/views/about.html"));
+});
+
+app.listen(8080);
